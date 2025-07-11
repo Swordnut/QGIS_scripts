9e37367f8943094584d83113ecc4bcbb085c709a
Swordnut <99604002+Swordnut@users.noreply.github.com>
2025-07-11 11:14:39 +0100

    Add files via upload

---
+++ batch_export_all_canvas_layers.py
@@ -0,0 +1,4 @@
+for vLayer in iface.mapCanvas().layers():
+    QgsVectorFileWriter.writeAsVectorFormat( vLayer, 
+        myDir + vLayer.name() + ".shp", "utf-8", 
+        vLayer.crs(), "ESRI Shapefile" )
